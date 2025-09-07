946a9f29005980acf9b974793cfb15f31f27bcc2
Praveen Singh <praveensingh9935250528@gmail.com>
2025-09-07 23:06:21 +0530

    first commit

---
+++ shoppinglyx/app/models.py
@@ -0,0 +1,105 @@
+#from distutils.command import upload
+from random import choices
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+STATE_CHOICES = (
+    ('Andaman & Nicobar Islands','Andaman & Nicobar Islands'),
+    ('Andhra Pradesh','Andhra Pradesh'),
+    ('Arunchal Pradesh','Arunchal Pradesh'),
+    ('Assam','Assam'),
+    ('Bihar','Bihar'),
+    ('Chandigarh','Chandigarh'),
+    ('Chhattisgarh','Chhattisgarh'),
+    ('Dadra & Nagar Havali','Dadra & Nagar Havali'),
+    ('Daman & Diu','Daman & Diu'),
+    ('Delhi','Delhi'),
+    ('Goa','Goa'),
+    ('Gujarat','Gujarat'),
+    ('Haryana','Haryana'),
+    ('Himanchal Pradesh','Himanchal Pradesh'),
+    ('Jambu & Kasmir','Jambu & Kasmir'),
+    ('Jharkhand','Jharkhand'),
+    ('Karnataka','Karnataka'),
+    ('kerala','kerala'),
+    ('Lakshadweep','Lakshadweep'),
+    ('Madhya Pradesh','Madhya Pradesh'),
+    ('Maharashtra','Maharashtra'),
+    ('Manipur','Manipur'),
+    ('Meghalaya','Meghalaya'),
+    ('Mizoram','Mizoram'),
+    ('Nagaland','Nagaland'),
+    ('Odisha','Odisha'),
+    ('Punducherry','Punducherry'),
+    ('Punjab','Punjab'),
+    ('Rajasthan','Rajasthan'),
+    ('Sikkim','Sikkim'),
+    ('Tamil Nadu','Tamil Nadu'),
+    ('Telangana','Telangana'),
+    ('Tripura','Tripura'),
+    ('Uttarakhand','Uttarakhand'),
+    ('Uttar Pradesh','Uttar Pradesh'),
+    ('West Bangal','West Bangal'),
+)
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES,max_length=50)
+
+    def __str__(self):
+        return str(self.id)
+
+CATEGORY_CHOICES = (
+        ('M', 'Mobile'),
+        ('L', 'Laptop'),
+        ('TW', 'Top Wear'),
+        ('BW', 'Borrom Wear'),
+    )
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    brand = models.CharField(max_length=100)
+    category = models.CharField(choices= CATEGORY_CHOICES, max_length=2)
+    product_image = models.ImageField(upload_to='producting')
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+    
+    @property
+    def total_cost(self):
+        return self.quantity * self.product.discounted_price
+
+
+STATUS_CHOICE = (
+    ('Accepted','Accepted'),
+    ('Packed','Packed'),
+    ('On The Way','On The Way'),
+    ('Delivered','Delivered'),
+    ('Cancel','Cancel'),
+)
+
+class OrderPlaced(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    ordered_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50, choices = STATUS_CHOICE, default="Order Placed...")
+
+    @property
+    def total_cost(self):
+        return self.quantity * self.product.discounted_price
